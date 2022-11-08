696a332f6eadcee787fbd6ca3a015a5d833ffcb8
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-08 17:53:48 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "9773cfc0-e60f-4d33-aabb-fc0a645ad789",
+    "home_feed": [
+        {
+            "id": "65a2551d-dee6-48e7-a67c-49e45611fe10",
+            "artist_name": "manage",
+            "home_carousel": [
+                {
+                    "id": "8a45a300-73b1-4f07-b356-a7ff01acd37c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/299.png"
+                },
+                {
+                    "id": "6ea5a9ff-0e69-4df2-9d1c-45f84e89f1df",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/271.png"
+                },
+                {
+                    "id": "6ce83238-64e2-46b2-9276-89c445c5c7f2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/139.png"
+                },
+                {
+                    "id": "6c4bd890-5555-4269-8e5e-0957b6c93881",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/344.png"
+                },
+                {
+                    "id": "0cf896da-fd56-469c-a26d-f198c4938221",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/234.png"
+                }
+            ],
+            "art_caption": "push",
+            "art_description": "Item forget only analysis perform. Condition adult they respond. Lead drive manage guess soldier seek add safe. Stand reflect very cold."
+        },
+        {
+            "id": "c4a1dbfe-31c8-4cac-ae71-066b84692bc2",
+            "artist_name": "beautiful",
+            "home_carousel": [
+                {
+                    "id": "c748b0eb-da71-41df-8fc4-f8b50e63fb0e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/113.png"
+                },
+                {
+                    "id": "20467288-27b0-4f45-afd2-f60b888afd22",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/231.png"
+                },
+                {
+                    "id": "43d2ac1b-54c9-4a21-9555-2920bd80c33c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/93.png"
+                },
+                {
+                    "id": "46d457ff-d965-4301-94d2-610543f8eac4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/338.png"
+                },
+                {
+                    "id": "78e59471-0d93-45f1-9999-a3b5d435e585",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/260.png"
+                }
+            ],
+            "art_caption": "teach",
+            "art_description": "Type boy million. Seek house worker class Congress begin."
+        },
+        {
+            "id": "f2c9db92-9aa0-41fe-8a48-971bd02e5e5c",
+            "artist_name": "nation",
+            "home_carousel": [
+                {
+                    "id": "d71d02c1-402d-412f-83c2-f0b5f1df5fae",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/197.png"
+                },
+                {
+                    "id": "d3d5c6dc-8955-451b-be44-a4b9bde0a7b6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/119.png"
+                },
+                {
+                    "id": "6dc92be2-3d80-40a8-8f23-245725b0ed93",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/175.png"
+                },
+                {
+                    "id": "505428f1-e7aa-4ec4-afde-d81d0c35f037",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/41.png"
+                },
+                {
+                    "id": "bc116df8-ff98-4063-a6a8-8fa428f9767f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/126.png"
+                }
+            ],
+            "art_caption": "left",
+            "art_description": "Box all job anything attorney scientist mouth and. Herself different brother land light expect."
+        },
+        {
+            "id": "10d53e2a-fe11-4b4e-891d-c7aa0ad0e2cc",
+            "artist_name": "sister",
+            "home_carousel": [
+                {
+                    "id": "70e1797d-baf6-4dd8-8a08-da2974dbcaa9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/93.png"
+                },
+                {
+                    "id": "90020dc0-9908-4900-8e31-c1c65a88912f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/223.png"
+                },
+                {
+                    "id": "2990e776-8092-4e66-94a8-f15844c681ac",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/319.png"
+                },
+                {
+                    "id": "493aec18-fe81-48f8-a9fe-3b9db14f8d0c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/278.png"
+                },
+                {
+                    "id": "d40c9191-23a5-41cb-acce-139c88b0dc85",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/358.png"
+                }
+            ],
+            "art_caption": "member",
+            "art_description": "Threat air peace trouble program name. Start field drive plant ask. Above memory each yard now nothing."
+        },
+        {
+            "id": "d65313cb-0a90-4ebc-adda-21239163ac96",
+            "artist_name": "parent",
+            "home_carousel": [
+                {
+                    "id": "7dfc098d-ad3a-44ce-9340-2500a194dbfd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/201.png"
+                },
+                {
+                    "id": "2a99b20a-23a4-406f-b61f-c32b7c5a20a8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/319.png"
+                },
+                {
+                    "id": "0a65ea5c-e76a-4a07-9d8b-e9d75ca48d8d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/191.png"
+                },
+                {
+                    "id": "63571b75-ddb8-4c64-b24d-01c3fda80577",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/328.png"
+                },
+                {
+                    "id": "89f811c4-f1c8-4da8-95bc-4802f7dc5c51",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/169.png"
+                }
+            ],
+            "art_caption": "although",
+            "art_description": "Of support citizen star. Into word wish teach specific magazine end. Little film until us guy political."
+        }
+    ]
+}
